b879206b2942f76268386aaa25d62fbe3d188662
naveenmech <vas.naveenkumar99@gmail.com>
2023-07-04 17:34:44 +0530

    add update on protected route

--- app/dashboard/page.js
+++ app/dashboard/page.js
@@ -2,6 +2,7 @@
 import Dashboard from "@/components/dashBoardPage/Dashboard";
 import Hero from "@/components/heroPage/Hero";
 import withAuth from "@/components/withAuthPage/withAuth";
+
 import React from "react";
 
 const page = () => {
